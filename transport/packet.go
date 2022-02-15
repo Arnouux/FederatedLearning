76c434f9971d59f204a2ed21c378e7cc627be5c6
Arthur Arnx <arthur.arnx@gmail.com>
2022-02-15 18:19:34 +0100

    Added types to nodes

--- transport/packet.go
+++ transport/packet.go
@@ -4,7 +4,13 @@ type Packet struct {
 	Source      string
 	Destination string
 	Message     string
+	Type        string
 }
+
+const (
+	EncryptedChunk = "encryptedChunk"
+	Ack            = "acknowlegdement"
+)
 
 func (p *Packet) String() string {
 	return "[" + p.Source + "] -> [" + p.Destination + "], message=\"" + p.Message + "\""
